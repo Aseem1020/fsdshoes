ba0903c8487860f0f5c7e339c54b15deb3dd603a
Aseem1020 <138798977+Aseem1020@users.noreply.github.com>
2024-10-12 09:35:58 +0530

    website

--- src/Component/Pages/OrderDetails/OrderDetails.jsx
+++ src/Component/Pages/OrderDetails/OrderDetails.jsx
@@ -57,7 +57,7 @@ const OrderDetails = () => {
                 <h3>{item.name}</h3>
                 <p>Price: ${item.price}</p>
                 <p>Quantity: {item.quantity}</p>
-                <p>Quantity: {item.size}</p>
+                <p>Size: {item.size}</p>
                 <p>Order ID: {item._id.slice(-4)}</p>
                 <p>Order Date: {item.orderDate.slice(0,10)}</p>
               </div>
